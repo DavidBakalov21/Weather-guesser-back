022426baefc7ea6f21c46fff9ad0a1e3a3271c09
DavidBakalov21 <davidbakalov33@gmail.com>
2024-09-21 12:43:35 +0300

    fix redis4

--- fastapi_backend.py
+++ fastapi_backend.py
@@ -14,7 +14,7 @@ from contextlib import asynccontextmanager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Create the Redis client and assign it to the app's state
-    redis_client = redis.Redis(host='redis://red-crn916m8ii6s73emn12g',port=6379, decode_responses=True)
+    redis_client = redis.Redis(host='redis://redis',port=6379, decode_responses=True)
    # redis_client = redis.Redis(host='redis',port=6379, decode_responses=True)
     app.state.redis = redis_client
     
